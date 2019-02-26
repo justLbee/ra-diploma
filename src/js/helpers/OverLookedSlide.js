aa88d6d7e896113e9d5e632e0cbe2a69c8495193
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2019-02-26 12:18:44 +0300

    added rerender ProductCard

--- src/js/helpers/OverLookedSlide.js
+++ src/js/helpers/OverLookedSlide.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import {Link} from "react-router-dom";
-import history from "./history"
+import {ProductCard} from "../ProductCard";
 
 const OverLookedSlide = ({product, display}) => {
   return (
-    product ?
+    Object.entries(product).length !== 0 && product.constructor === Object  ?
     <div className={`overlooked-slider__item`}
          style={{background: `url(${product.images[0]}) no-repeat`, backgroundSize: 'cover', display: display}}>
       <Link to={`/product/${product.id}`}
-            key={product.id} />
+            key={product.id} onClick={productChanged}/>
     </div> : null
   )
 };
+
+function productChanged() {
+  const productCard = new ProductCard();
+  productCard.getProductData();
+}
+
 
 export default OverLookedSlide
